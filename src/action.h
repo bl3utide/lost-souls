3c5f25943e0b4cf1db55550913eeb6444a300fae
bl3utide <bl3utide@gmail.com>
2024-11-22 02:08:28 +0900

    add func output newline to action base class

--- src/action.h
+++ src/action.h
@@ -1,9 +1,15 @@
 #ifndef ACTION_H
 #define ACTION_H
 
+#include <iostream>
+
 class ActionBase {
 public:
     virtual void execute() const = 0;
+
+    void nl() const noexcept {
+        std::cout << std::endl;
+    }
 };
 
 #endif // ACTION_H
